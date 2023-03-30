c3804161c10d4ffdc2e45bb9cf5aada3a8d9ddd6
SRI <SRI@DESKTOP-9D5MQB8>
2023-03-30 18:18:31 +0530

    BDD Framework Created

---
+++ src/main/java/BaseLayer/Baseclass.java
@@ -0,0 +1,10 @@
+package BaseLayer;
+
+public class Baseclass {
+
+	public static void main(String[] args) {
+      
+		System.out.println("Base Class Is Created");
+	}
+
+}
---
+++ src/test/java/StepDef/LoginPage.java
@@ -0,0 +1,11 @@
+package StepDef;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Login Page");	
+		
+	}
+
+}
